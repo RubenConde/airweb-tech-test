38f40b63b8bb1b10eecad2416c170988b91136f7
Rubén Conde Magaña <code@rubn.dev>
2023-08-21 15:17:07 +0200

    refactor: Get categories from db
    Signed-off-by: Rubén Conde Magaña <code@rubn.dev>

--- src/config/database/sqlite.config.ts
+++ src/config/database/sqlite.config.ts
@@ -1,11 +1,12 @@
 import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import { Category } from 'src/models/category/entity/category.entity';
 import { User } from 'src/models/user/entity/user.entity';
 
 export const sqliteConfig: TypeOrmModuleAsyncOptions = {
    useFactory: () => ({
       type: 'sqlite',
       database: String(process.env.DATABASE_NAME),
-      entities: [User],
+      entities: [User, Category],
       synchronize: process.env.NODE_ENV === 'development',
    }),
 };
