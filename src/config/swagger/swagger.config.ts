c03b82451563f36e278000121f1f67eb8279b5b4
Rubén Conde Magaña <code@rubn.dev>
2023-08-22 14:40:42 +0200

    refactor: Run instructions
    Signed-off-by: Rubén Conde Magaña <code@rubn.dev>

--- src/config/swagger/swagger.config.ts
+++ src/config/swagger/swagger.config.ts
@@ -2,17 +2,9 @@ import { DocumentBuilder, SwaggerCustomOptions, SwaggerDocumentOptions } from '@
 
 const swaggerConfiguration = new DocumentBuilder()
    .addBearerAuth({ scheme: 'bearer', type: 'http' })
-   .setContact('Contact', String(process.env.SITE_URL), String(process.env.CONTACT_EMAIL))
    .setDescription(`API REST for ${process.env.API_NAME}`)
    .setTitle(String(process.env.API_NAME))
    .setVersion(String(process.env.npm_package_version));
-
-if (process.env.NODE_ENV === 'development') {
-   swaggerConfiguration.addServer(`http://localhost:${process.env.PORT}`);
-}
-if (process.env.NODE_ENV === 'production') {
-   swaggerConfiguration.addServer(String(process.env.SERVER_URL));
-}
 
 export const swaggerConfig = swaggerConfiguration.build();
 
